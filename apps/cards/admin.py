0bdb403d5e104d9fc9de54f5adcb4788cc19e4fc
Marsel69 <zulpukarovmarsel30@gmail.com>
2023-09-19 17:59:53 +0600

    add Favorite, Basket, Comment, Category, Subcategory

--- apps/cards/admin.py
+++ apps/cards/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from apps.cards.models import *
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(SubCategory)
