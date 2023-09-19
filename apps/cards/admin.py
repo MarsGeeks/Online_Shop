d3fe052763eb5ff9127f3c22489ea9caf3a0db86
Marsel69 <zulpukarovmarsel30@gmail.com>
2023-09-19 21:30:22 +0600

    add favorite, remove favorite

--- apps/cards/admin.py
+++ apps/cards/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from apps.cards.models import *
 # Register your models here.
-admin.site.register(Card)
+admin.site.register(Product)
 admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(SubCategory)
