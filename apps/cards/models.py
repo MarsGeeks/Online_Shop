b8d95e2c57e06cb32b5fd2fdcbf994a9bc844e1f
Marsel69 <zulpukarovmarsel30@gmail.com>
2023-09-19 18:02:36 +0600

    add Favorite, Basket, Comment, Category, Subcategory

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.users import models as user_models
 
 # Create your models here.
 class Card(models.Model):
@@ -8,3 +9,62 @@ class Card(models.Model):
     price = models.PositiveIntegerField(default=0)
     rating = models.PositiveIntegerField()
     
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Карточка"
+        verbose_name_plural = "Карточки"
+
+class UserFavoriteCard(models.Model):
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, verbose_name="Товар")
+
+    class Meta:
+        verbose_name = "Избранное"
+        verbose_name_plural = "Избранные"
+
+    def __str__(self):
+        user_full_name = self.user.get_full_name()
+        return f"{user_full_name} - {self.card.title}"
+
+class Basket(models.Model):
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, verbose_name="Товар")
+
+    def __str__(self):
+        user_full_name = self.user.get_full_name()
+        return f"{user_full_name} - {self.card.title}"
+
+class Comment(models.Model):
+    text = models.CharField(max_length=300)
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Коментария"
+        verbose_name_plural = "Коментарии"
+
+class Category(models.Model):
+    title = models.CharField(null=True, max_length=100, verbose_name='Названия')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                  related_name='subcategories', verbose_name='Категория')
+    title = models.CharField(max_length=100, verbose_name="Названия")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Под категории"
+        verbose_name_plural = "Под категории"
+
