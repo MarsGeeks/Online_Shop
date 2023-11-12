b4a58326395dd225b90e2dd8a0991724d34f1db9
Marsel69 <zulpukarovmarsel30@gmail.com>
2023-11-12 14:23:14 +0600

    characteristics, delivery

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -25,6 +25,20 @@ class SubCategory(models.Model):
     class Meta:
         verbose_name = "Под категории"
         verbose_name_plural = "Под категории"
+class Characteristic(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название характеристики")
+
+    def __str__(self):
+        return self.name
+
+
+class Delivery(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Способ доставки")
+    days = models.PositiveIntegerField(verbose_name="Дни доставки")
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, verbose_name="Автор")
@@ -34,16 +48,30 @@ class Product(models.Model):
     subcategory = models.ManyToManyField(SubCategory, verbose_name="Категории")
     rating = models.IntegerField(choices=RATING, default=0)
 
+    characteristics = models.ManyToManyField(Characteristic, through='ProductCharacteristic',
+                                             verbose_name="Характеристики")
+    delivery = models.ForeignKey(Delivery, on_delete=models.SET_NULL, null=True, blank=True,
+                                 verbose_name="Способ доставки")
+
     @property
     def average_rating(self):
         average = Product.objects.aggregate(avg_rating=Avg('rating')).get('avg_rating') or 0
         return min(5, max(0, average))
+
     def __str__(self):
         return self.title
-    
+
     class Meta:
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
+
+class ProductCharacteristic(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.product.title} - {self.characteristic.name}: {self.value}"
 
 class UserFavoriteProduct(models.Model):
     user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, verbose_name="Пользователь")
